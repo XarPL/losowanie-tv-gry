5cd798f584a96cb6af0c7b9b66f0265cd14ed4b0
Xardas49 <XardasTheModder@interia.pl>
2018-08-13 22:18:31 +0200

    update everything

--- js/main.js
+++ js/main.js
@@ -12,23 +12,49 @@ var los3 = true;
 var time = 0;
 var modal = document.getElementById('myModal');
 var modal_close = document.getElementsByClassName("close")[0];
+var losowania = 0;
+var specjalne = 0;
+if (localStorage.losowania) {
+    losowania = Number(localStorage.losowania);
+	specjalne = Number(localStorage.specjalne);
+}
 document.getElementById("keywords").innerHTML = "baza danych maszyny zawiera "+ (w1.length+w2.length) + " pozycji ("+(9*w1.length*w2.length)+" kombinacji)";
-function screen()
+document.getElementById("stats").innerHTML = "twoje losowania: "+ losowania + ", wylosowałeś/aś "+ specjalne + " specjalnych tematów";
+var started = false;
+function download()
 {
-	modal.style.display = "block";
+	
 	document.querySelector("#img01").innerHTML = "";
 	html2canvas(document.querySelector("#machine")).then(canvas => {
-    document.querySelector("#img01").appendChild(canvas)
+	canvas.toBlob(function(blob) {saveAs(blob, "losowanko.png");});
+	});
+}
+function preview()
+{
+	modal.style.display = "block";
+	var div = document.querySelector("#img01");
+	document.querySelector("#img01").innerHTML = "";
+	html2canvas(document.querySelector("#machine")).then(canvas => {
+    div.appendChild(canvas)
 	});
 }
 function start()
 {
-	document.getElementById("b1").innerHTML = "";
-	document.getElementById("b2").innerHTML = "";
-	document.getElementById("b3").innerHTML = "";
-	document.getElementById("a1").play();
-	lottSpeed = 50;lottTime = 600;los1 = true;los2 = true;los3 = true;time = 0;
-	setTimeout(losowanko, 3300);
+	if (started == false)
+	{
+		started = true;
+		document.getElementById("a3").pause();
+		document.getElementById("a4").pause();
+		document.getElementById("a3").currentTime = 0;
+		document.getElementById("a4").currentTime = 0;
+		document.getElementById("b1").innerHTML = "";
+		document.getElementById("b2").innerHTML = "";
+		document.getElementById("b3").innerHTML = "";
+		document.getElementById("a1").play();
+		lottSpeed = 50;lottTime = 600;los1 = true;los2 = true;los3 = true;time = 0;
+		setTimeout(losowanko, 3300);
+		
+	};
 }
 function losowanko ()
 {
@@ -40,6 +66,7 @@ function losowanko ()
 	document.getElementById("b2").innerHTML = w1[Math.floor(getRandomArbitrary(0, w1.length))];
 	if (los3)
 	document.getElementById("b3").innerHTML = w2[Math.floor(getRandomArbitrary(0, w2.length))];
+	
 	time += 1;
 	if(time == 80)
 		los1 = false;
@@ -53,7 +80,22 @@ function losowanko ()
 		setTimeout(losowanko, lottSpeed);
 	}else{
 		document.getElementById("progress").innerHTML = "losowanko zakonczone, jeszcze raz?";
-		document.getElementById("a3").play();
+		if (document.getElementById("b2").innerHTML == w1[7] && document.getElementById("b3").innerHTML == w2[21])
+		{
+			document.getElementById("a4").play();
+			document.getElementById("progress").innerHTML = "oryginał: <a href='https://www.youtube.com/watch?v=yvGXCisAaR4'>Link do YT</a><br />losowanko zakonczone, jeszcze raz?";
+			specjalne += 1;
+		}
+		else{
+			document.getElementById("a3").play();
+		}
+		document.getElementById("a2").pause();
+		document.getElementById("a2").currentTime = 0;
+		started = false;
+		losowania += 1;
+		document.getElementById("stats").innerHTML = "twoje losowania: "+ losowania + ", wylosowałeś/aś "+ specjalne + " specjalnych tematów";
+		localStorage.losowania = losowania;
+		localStorage.specjalne = specjalne;
 	}
 }
 modal_close.onclick = function() { 
