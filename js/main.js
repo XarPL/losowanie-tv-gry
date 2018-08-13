c6a22ba58156e7926d86c3884a912afd2a19af04
Xardas49 <XardasTheModder@interia.pl>
2018-08-13 18:23:29 +0200

    update everything

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 ﻿function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
 }
-var w1 = ["najlepszych", "chinskich", "polskich", "niemieckich", "rosyjskich", "największych", "najmniejszych", "najsłodszych", "najgłupszych", "losowych", "skandalicznych", "nieprzewidywalnych", "erotycznych", "najgorszych", "podrabianych", "najsłabszych", "najsilniejszych"];
-var w2 = ["cytryn w grach", "bajek", "modów", "gier", "filmów", "błędów", "momentów w grach", "reklam", "reklam delmy", "bossów", "owoców", "broni", "pomidorów"];
+var w1 = ["najlepszych", "chinskich", "polskich", "niemieckich", "rosyjskich", "największych", "najmniejszych", "najsłodszych", "najgłupszych", "losowych", "skandalicznych", "nieprzewidywalnych", "przewidywalnych", "erotycznych", "najgorszych", "podrabianych", "najsłabszych", "najsilniejszych", "najpiękniejszych", "walecznych", "najkrótszych", "najdłuższych"];
+var w2 = ["cytryn w grach", "bajek", "modów", "gier", "filmów", "błędów", "momentów w grach", "reklam", "reklam delmy", "bossów", "owoców", "broni", "pomidorów", "rozłamów w ekipach", "mordobitek", "strzelanek", "misji hitmena", "aktorów", "twórców","menju w grach", "kłamstw deweloperów","kłamstw Todda Howarda", "modów do Skyrim", "modów do Gothic","modów do Wiedźmin 3", "dublaży w grach", "dublaży w filmach", "pomówień", "muzyczek w grach", "muzyczek w filmach", "bananów"];
 
 var lottSpeed = 50;
 var lottTime = 600;
@@ -10,6 +10,17 @@ var los1 = true;
 var los2 = true;
 var los3 = true;
 var time = 0;
+var modal = document.getElementById('myModal');
+var modal_close = document.getElementsByClassName("close")[0];
+document.getElementById("keywords").innerHTML = "baza danych maszyny zawiera "+ (w1.length+w2.length) + " pozycji ("+(9*w1.length*w2.length)+" kombinacji)";
+function screen()
+{
+	modal.style.display = "block";
+	document.querySelector("#img01").innerHTML = "";
+	html2canvas(document.querySelector("#machine")).then(canvas => {
+    document.querySelector("#img01").appendChild(canvas)
+	});
+}
 function start()
 {
 	document.getElementById("b1").innerHTML = "";
@@ -38,10 +49,13 @@ function losowanko ()
 		los3 = false;
 	if (los3 == true)
 	{
-		document.getElementById("progress").innerHTML = "trwa losowanko (" + Math.floor((lottSpeed/lottTime)*100) + "%) " +time;
+		document.getElementById("progress").innerHTML = "trwa losowanko (" + Math.floor((time/120)*100) + "%)";
 		setTimeout(losowanko, lottSpeed);
 	}else{
 		document.getElementById("progress").innerHTML = "losowanko zakonczone, jeszcze raz?";
 		document.getElementById("a3").play();
 	}
+}
+modal_close.onclick = function() { 
+  modal.style.display = "none";
 }
